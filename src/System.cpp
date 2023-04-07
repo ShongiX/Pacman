c0467653cdfa2daa9904482bc12666b79c9a19da
Csongi <56769659+ShongiX@users.noreply.github.com>
2023-04-07 12:47:12 +0200

    Initial Commit

---
+++ src/System.cpp
@@ -0,0 +1,5 @@
+//
+// Created by csong on 4/7/2023.
+//
+
+#include "System.hpp"
---
+++ src/System.hpp
@@ -0,0 +1,14 @@
+//
+// Created by csong on 4/7/2023.
+//
+
+#ifndef PACMAN_SYSTEM_HPP
+#define PACMAN_SYSTEM_HPP
+
+
+class System {
+
+};
+
+
+#endif //PACMAN_SYSTEM_HPP
